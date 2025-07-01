b939d3359d813b6b808a4a18aa65b25535d93a9f
daticalamy <50338582+daticalamy@users.noreply.github.com>
2025-06-30 21:01:07 -0500

    Dev build (#3)
    * Adjusting changelog
    
    * Updating schemas
    
    * Removed default schema for SqlServer
    
    * Added new checks file
    
    * Removed scripts
    
    * Send reports to S3
    
    * Removed s3 for reports
    
    * New S3 folder
    
    ---------
    
    Co-authored-by: Amy Smith <asmith@liquibase.com>

---
+++ changelogs/v0.1.0/sales_contacts.sql
@@ -0,0 +1,14 @@
+--liquibase formatted sql
+
+--changeset amy.smith:sales_contacts labels:v0.1.0
+create table Sales.Contacts (
+  id int, 
+  name varchar(30),
+  dept varchar(30) 
+);
+--rollback DROP TABLE Sales.Contacts;
+
+
+--changeset amy.smith:sales_contacts_idx labels:v0.1.0
+CREATE UNIQUE INDEX name_idx ON Sales.Contacts (name);
+--rollback DROP INDEX name_idx on Sales.Contacts;
