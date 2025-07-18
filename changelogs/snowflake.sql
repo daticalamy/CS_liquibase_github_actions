1e945a7ff83c4fe26d5656ba2804bc5d79597cd6
Amy Smith <asmith@liquibase.com>
2025-07-18 11:21:40 -0500

    Removed schema

--- changelogs/snowflake.sql
+++ changelogs/snowflake.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 
 --changeset amy.smith:sales_contacts labels:v0.1.0
-create table Sales.Contacts (
+create table Contacts (
   id int, 
   name varchar(30),
   dept varchar(30) 
 );
---rollback DROP TABLE Sales.Contacts;
+--rollback DROP TABLE Contacts;
 
 --changeset amy.smith:sales_contacts_idx labels:v0.1.0
-CREATE UNIQUE INDEX name_idx ON Sales.Contacts (name);
---rollback DROP INDEX name_idx on Sales.Contacts;
+CREATE UNIQUE INDEX name_idx ON Contacts (name);
+--rollback DROP INDEX name_idx on Contacts;
