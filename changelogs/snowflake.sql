3e7bf58f455336298aa22118cc476164adb647a3
Amy Smith <asmith@liquibase.com>
2025-07-20 19:49:13 -0500

    Adding insert

--- changelogs/snowflake.sql
+++ changelogs/snowflake.sql
@@ -8,3 +8,8 @@ CREATE TABLE Contacts (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 --rollback DROP TABLE Contacts;
+
+--changeset amy.smith:sales_insert_01 labels:v0.1.1
+INSERT INTO Contacts (id, name, email) VALUES (1, 'Amy Smith', 'asmith@liquibase.com');
+INSERT INTO Contacts (id, name, email) VALUES (2, 'Ben Riley', 'briley@liquibase.com');
+--rollback DELETE FROM Contacts;
