f4738573e3c355466e71ef751a1d54fb665d2f57
Amy Smith <asmith@liquibase.com>
2025-07-24 16:19:01 -0500

    Removed dups

--- changelogs/issues/ABC-0001/001.sql.sql
+++ changelogs/issues/ABC-0001/001.sql.sql
@@ -1,36 +1,36 @@
 --liquibase formatted sql
 
 --changeset amy.smith:sales_contacts labels:v0.1.0
-create table Sales.Contacts (
+create table Sales.Contacts2 (
   id int, 
   name varchar(30),
   dept varchar(30) 
 );
---rollback DROP TABLE Sales.Contacts;
+--rollback DROP TABLE Sales.Contacts2;
 
 
 --changeset amy.smith:sales_contacts_idx labels:v0.1.0
-CREATE UNIQUE INDEX name_idx ON Sales.Contacts (name);
---rollback DROP INDEX name_idx on Sales.Contacts;
+CREATE UNIQUE INDEX name2_idx ON Sales.Contacts2 (name);
+--rollback DROP INDEX name_idx on Sales.Contacts2;
 
 --changeset amy.smith:stores
-create table Sales.Stores (
+create table Sales.Stores2 (
   id int, 
   name varchar(30),
   city varchar(30) 
 );
---rollback DROP TABLE Sales.Stores;
+--rollback DROP TABLE Sales.Stores2;
 
 --changeset amy.smith:goals
-create table Sales.Goals (
+create table Sales.Goals2 (
   id int, 
   name varchar(30) 
 );
---rollback DROP TABLE Sales.Goals;
+--rollback DROP TABLE Sales.Goals2;
 
 --changeset amy.smith:kpis
-create table Sales.Kpis (
+create table Sales.Kpis2 (
   id int, 
   name varchar(30) 
 );
---rollback DROP TABLE Sales.Kpis;
+--rollback DROP TABLE Sales.Kpis2;
