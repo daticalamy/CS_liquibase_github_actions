02a6e298f89df4f97318ad2c87638c3eedc435b8
Amy Smith <asmith@liquibase.com>
2025-07-25 14:37:58 -0500

    Adding 6

--- changelogs/issues/ABC-0006/002.sql
+++ changelogs/issues/ABC-0006/002.sql
@@ -1,8 +1,8 @@
 --liquibase formatted sql
 
---changeset amy.smith:table05a labels:abc-0005 
-create table Sales.table05a (
+--changeset amy.smith:table06a
+create table Sales.table06a (
   id int, 
   name varchar(30)
 );
---rollback DROP TABLE Sales.table05a;
+--rollback DROP TABLE Sales.table06a;
