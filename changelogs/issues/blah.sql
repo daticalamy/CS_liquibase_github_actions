5ceac286579c0c639746fd9e7578a7afc209f438
Amy Smith <asmith@liquibase.com>
2025-07-30 10:05:13 -0500

    Changed trigger

--- changelogs/issues/blah.sql
+++ changelogs/issues/blah.sql
@@ -10,7 +10,6 @@ create table Sales.table_blah (
 --changeset amy.smith:table_blah2 
 create table Sales.table_blah2 (
   id int, 
-  name varchar(30),
   name varchar(30)
 );
 --rollback DROP TABLE Sales.table_blah2;
