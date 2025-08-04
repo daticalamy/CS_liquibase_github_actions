6e118c8c37d36cc9d2416d0711d6102453f1d98b
daticalamy <50338582+daticalamy@users.noreply.github.com>
2025-08-04 11:13:34 -0500

    Testing (#23)
    * Resetting changelog
    
    * Error if empty
    
    * Adding changes
    
    * Added Liquibase labels to pending changesets
    
    * Update 001.sql
    
    * Update liquibase-rollback.flowfile.yaml
    
    * Update liquibase-rollback.flowfile.yaml
    
    * Update liquibase-rollback.flowfile.yaml
    
    ---------
    
    Co-authored-by: Amy Smith <asmith@liquibase.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- changelogs/issues-old/ABC-0001/001.sql
+++ changelogs/issues-old/ABC-0001/001.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset amy.smith:sales_contacts labels:abc-0001 
+--changeset amy.smith:sales_contacts labels:v0.1.0,abc-0001
 create table Sales.Contacts2 (
   id int, 
   name varchar(30),
@@ -9,7 +9,7 @@ create table Sales.Contacts2 (
 --rollback DROP TABLE Sales.Contacts2;
 
 
---changeset amy.smith:sales_contacts_idx labels:abc-0001
+--changeset amy.smith:sales_contacts_idx labels:v0.1.0,abc-0001
 CREATE UNIQUE INDEX name2_idx ON Sales.Contacts2 (name);
 --rollback DROP INDEX name2_idx on Sales.Contacts2;
 
