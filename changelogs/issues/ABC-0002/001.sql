54b7f50b607b744816c4043bab95c13485e4f6f9
Amy Smith <asmith@liquibase.com>
2025-08-05 15:58:34 -0500

    6

--- changelogs/issues/ABC-0002/001.sql
+++ changelogs/issues/ABC-0002/001.sql
@@ -7,3 +7,11 @@ create table Sales.Contacts5 (
   dept varchar(30) 
 );
 --rollback DROP TABLE Sales.Contacts5;
+
+--changeset amy.smith:sales_contacts6 labels:abc-0002 
+create table Sales.Contacts6 (
+  id int, 
+  name varchar(30),
+  dept varchar(30) 
+);
+--rollback DROP TABLE Sales.Contacts6;
