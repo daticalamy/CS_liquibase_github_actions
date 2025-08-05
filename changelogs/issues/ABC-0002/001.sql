42230214ad098bf77481844345b57a47f5ff982c
Amy Smith <asmith@liquibase.com>
2025-08-05 15:48:38 -0500

    New change

---
+++ changelogs/issues/ABC-0002/001.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+
+--changeset amy.smith:sales_contacts5
+create table Sales.Contacts5 (
+  id int, 
+  name varchar(30),
+  dept varchar(30) 
+);
+--rollback DROP TABLE Sales.Contacts5;
